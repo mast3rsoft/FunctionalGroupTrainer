2f399e1969a4d304f71b59761f412bcb21672ab7
jakob <jakob.n.neufeld@gmail.com>
2020-02-22 18:06:33 +0100

    Initial Commit

---
+++ FunctionalGroupTrainer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FunctionalGroupTrainer
+//
+//  Created by Niko Neufeld on 2/22/20.
+//  Copyright © 2020 Niko Neufeld. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
